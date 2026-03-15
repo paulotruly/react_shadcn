ac0074e4bcf8a86eeb3befb0f2ddf8c7c2f240df
Paulo <paulomsmsms@gmail.com>
2026-03-15 14:45:09 -0300

    dashboardtologin

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,8 +1,20 @@
 import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 function Dashboard() {
 
   const navigate = useNavigate()
+  const token = localStorage.getItem("accessToken")
+
+  useEffect(() => {
+      if (!token) {
+        navigate("/login", { replace: true })
+      }
+    }, [token, navigate])
+  
+    if (token) {
+      return null
+    }
 
   const handleLogout = () => {
     localStorage.removeItem("accessToken")
